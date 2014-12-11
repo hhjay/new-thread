04f09a3bf84ccea60c8ff34d1c7d1a7210972fe2
hhjay <sunhhjay@gmail.com>
2014-12-11 15:13:26 +0800

    uptada zhaoxin/js

--- zhaoxin/js/index.js
+++ zhaoxin/js/index.js
@@ -2,45 +2,22 @@
 ©sunhhjay@gmail.com
 */
 $(document).ready(function(){
-	$(".toteamIn").click(function(){//团队介绍
-		$("#teamIn").fadeIn();
+	var showArr = {
+		"toteamIn"	:"#teamIn",
+		"toachieve"	:"#achieve",
+		"tonowPro"	:"#nowPro",
+		"toproIn"	:"#proIn",
+		"tohezuoQy"	:"#hezuoQy"
+	};
+	var str = "";
+	$(".nav img").click(function(){
+		str = $(this).attr('class');
+		console.log(showArr[str]);
+		$(showArr[str]).fadeIn();
 	});
-	$(".tiClose").click(function(){
-    	$("#teamIn").fadeOut();
- 	});
 
- 	$(".toachieve").click(function(){//成果展示
-		$("#achieve").fadeIn();
-	});
 	$(".tiClose").click(function(){
-    	$("#achieve").fadeOut();
- 	});
-
-	$(".tonowPro").click(function(){//在做项目
-		$("#nowPro").fadeIn();
-	});
-	$(".tiClose").click(function(){
-    	$("#nowPro").fadeOut();
- 	});
-
-	$(".toproIn").click(function(){//项目介绍
-		$("#proIn").fadeIn();
-	});
-	$(".tiClose").click(function(){
-    	$("#proIn").fadeOut();
- 	});
-
-	$(".tohezuoTeam").click(function(){//合作团队
-		$("#hezuoTeam").fadeIn();
-	});
-	$(".tiClose").click(function(){
-    	$("#hezuoTeam").fadeOut();
- 	});
-	$(".tohezuoQy").click(function(){//合作企业
-		$("#hezuoQy").fadeIn();
-	});
-	$(".tiClose").click(function(){
-    	$("#hezuoQy").fadeOut();
+    	console.log($(this).parent().fadeOut());
  	});
 });	
 
